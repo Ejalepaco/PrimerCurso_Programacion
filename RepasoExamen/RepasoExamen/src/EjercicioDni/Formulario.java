b17bbdb88c7600dd5548cde5575cf72744db8a3d
Ejalepaco <fdominguezp@gmail.com>
2024-06-08 22:03:33 +0200

    Formulario DNI

---
+++ RepasoExamen/RepasoExamen/src/EjercicioDni/Formulario.java
@@ -0,0 +1,74 @@
+package EjercicioDni;
+
+import EjercicioFormulario.LongitudDniNoValidaException;
+
+import java.util.Scanner;
+
+public class Formulario {
+
+    public static void main(String[] args) {
+
+        System.out.println("Comenzamos el ejercicio del formulario DNI");
+
+        Scanner scanner = new Scanner(System.in);
+
+        String nombre = null;
+        String apellido = null;
+        String dni = null;
+        int opcion = 0;
+        boolean datosCompletos = false;
+
+        do {
+            try {
+
+
+                System.out.println("""
+                        1. Introduce nombre
+                        2. Introduce apellido
+                        3. Introduce dni
+                        4. Finalizar
+                        """);
+                System.out.println("Elige una opción");
+                opcion = scanner.nextInt();
+
+
+                switch (opcion) {
+                    case 1:
+                        System.out.println("introduzca el nombre");
+                        nombre = scanner.next();
+                        break;
+                    case 2:
+                        System.out.println("Introduce el apellido");
+                        apellido = scanner.next();
+                        break;
+                    case 3:
+                        System.out.println("Introduce el dni");
+                        dni = scanner.next();
+
+                        if (dni.length() != 9) {
+                            throw new LongitudDniNoValidaException("Longitud no valida");
+                        }
+                        break;
+                    case 4:
+                        if (nombre != null && apellido != null && dni != null) {
+                            System.out.println("Saliendo");
+                            datosCompletos = true;
+                            break;
+                        } else {
+                            System.out.println("No puedes salir sin rellenar todo");
+                        }
+                }
+            } catch (LongitudDniNoValidaException e) {
+                System.err.println(e.getMessage());
+
+            }
+        } while (!datosCompletos);
+
+        System.out.println(nombre);
+        System.out.println(apellido);
+        System.out.println(dni);
+
+    }
+    }
+
+
